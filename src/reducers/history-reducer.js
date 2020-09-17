2ad91fda29aae2249ec5f5dca2e9efa1a00a2a70
Sara Kane <kane.sarak@gmail.com>
2020-09-17 10:52:18 -0700

    Divide app into component files
    Co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

---
+++ src/reducers/history-reducer.js
@@ -0,0 +1,14 @@
+export default (state = [{   squares: Array(9).fill(null), }], action) => {
+  const { history, id } = action;
+  switch (action.type) {
+  case 'ADD_HISTORY':
+    return Object.assign({}, state, {
+      [id] : {
+        history: history,
+        id: id
+      }
+    });
+    default:
+      return state;
+  }
+};
